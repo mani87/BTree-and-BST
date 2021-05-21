84bd47385d477346163b3126e44218d315f83012
Mani Kumar <mk0872191152@gmail.com>
2021-05-22 00:48:52 +0530

    [BST] All functions implemented

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -99,7 +99,92 @@ Node* inOrderPred(Node* node) {
 
 // smallest value greater than or equal to the key
 Node* ceiling(Node* root, string key) {
+	Node* c = NULL;
 
+	while(root) {
+		if(root->data == key) {
+			c = root;
+			break;
+		} 
+		if(root->data > key) {
+			//we move left
+			c = root;
+			root = root->left;
+		} else if(root->data < key) {
+			root = root->right;
+		}
+	}
+
+	return c;
+}
+
+
+// greatest value smaller than or equal to the key
+Node* floorVal(Node * root, string key) {
+	Node* f = NULL;
+
+	while(root) {
+		if(root->data == key) {
+			f = root;
+			break;
+		}
+
+		if(root->data < key) {
+			f = root;
+			root = root->right;
+		} else if(root->data > key) {
+			root = root->left;
+		}
+	}
+	return f;
+}
+
+
+
+Node* Delete(Node* root, string key) {
+
+	if(!root) return NULL;
+
+	if(root->data > key) root->left = Delete(root->left, key);
+	else if(root->data < key) root->right = Delete(root->right, key);
+	else {
+		// we have found the node
+		if(!root->left && !root->right) {
+			// leaf node
+			delete root;
+			root = NULL;
+		} else if(!root->left) {
+			Node* node = root;
+			root = root->right;
+			delete node;
+		} else if(!root->right) {
+			Node* node = root;
+			root = root->left;
+			delete node;
+		} else {
+			Node* temp = treeMinimum(root->right);
+			root->data = temp->data;
+			root->right = Delete(root->right, temp->data);
+		}
+	}
+	return root;
+}
+
+
+void preOrder(Node* root) {
+	if(!root) return;
+
+	stack<Node*> st;
+	st.push(root);
+
+	while(!st.empty()) {
+		Node* temp = st.top();
+		cout<<temp->data<<" ";
+
+		st.pop();
+		if(temp->right) st.push(temp->right);
+		if(temp->left) st.push(temp->left);
+	}
 }
 
 
@@ -129,9 +214,16 @@ int main() {
 
 	cout<<"Min: "<<min_->data<<endl;
 	cout<<"Max: "<<max_->data<<endl;
-	cout<<"Parent of 2 is "<<(recTreeSearch(root, "2")->parent)->data<<endl;
-	cout<<"InOrderSuccessor of 2 is "<<inOrderSuccessor(recTreeSearch(root, "2"))->data<<endl;
-	cout<<"InOrderPredecessor of 4 is "<<inOrderPred(recTreeSearch(root, "4"))->data<<endl;
+	cout<<"Parent of 5 is "<<(recTreeSearch(root, "5")->parent)->data<<endl;
+	cout<<"InOrderSuccessor of 3 is "<<inOrderSuccessor(recTreeSearch(root, "3"))->data<<endl;
+	cout<<"InOrderPredecessor of 5 is "<<inOrderPred(recTreeSearch(root, "5"))->data<<endl;
+	cout<<"Cieling value of 4 is "<<ceiling(root, "4")->data<<endl;
+	cout<<"Floor value of 4 is "<<floorVal(root, "4")->data<<endl;
+	preOrder(root);
+	root = Delete(root, "5");
+	cout<<endl;
+	preOrder(root);
+	cout<<endl;
 
 	return 0;
 }
